852b4b6d3691a8dd887412e6e66ee4e297b21d42
Morgan <morganfwilliams@gmail.com>
2019-12-27 14:49:22 -0500

    spice it up with js in md

---
+++ src/components/Seriously.svelte
@@ -0,0 +1,24 @@
+
+<div><slot></slot></div>
+
+<style>
+	div {
+		background: pink;
+		border: 23px solid orange;
+		padding: 0 15px;
+		width: 400px;
+		text-align: center;
+		transform: translateX(-50px);
+		animation: 2s slide infinite alternate ease-in-out;
+	}
+
+	@keyframes slide {
+		from {
+			transform: translateX(-50px)
+		}
+		to {
+			transform: translateX(50px)
+		}
+
+	}
+</style>
