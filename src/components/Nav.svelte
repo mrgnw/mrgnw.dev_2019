32f9f08ea40b032ecef4ab6b2662bec334a94b39
Morgan <morganfwilliams@gmail.com>
2019-12-27 13:38:43 -0500

    Feelin .svexy

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -52,6 +52,7 @@
 	<ul>
 		<li><a class:selected='{segment === undefined}' href='.'>home</a></li>
 		<li><a class:selected='{segment === "about"}' href='about'>about</a></li>
+		<li><a href='mdsvex'>svexy</a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
