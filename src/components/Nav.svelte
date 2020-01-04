6e95f78775db2e38823f7891bfb5b58edd70b5a2
Morgan <morganfwilliams@gmail.com>
2020-01-04 17:59:40 -0500

    remove home

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -50,7 +50,6 @@
 
 <nav>
 	<ul>
-		<li><a class:selected='{segment === undefined}' href='.'>home</a></li>
 		<li><a class:selected='{segment === undefined}' href='.'>mrgnw.dev</a></li>
 		<li><a class:selected='{segment === "about"}' href='about'>about</a></li>
 		<!-- <li><Icon
