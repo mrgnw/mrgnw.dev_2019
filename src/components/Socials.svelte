4f30076ff891b3e6484c5409c5cfec298e1181de
Morgan <morganfwilliams@gmail.com>
2020-01-16 15:00:06 -0500

    icons broke netlify build

--- src/components/Socials.svelte
+++ src/components/Socials.svelte
@@ -1,27 +1,12 @@
 <script>
-import IoLogoInstagram from 'svelte-icons/io/IoLogoInstagram.svelte'
-import IoLogoTwitter from 'svelte-icons/io/IoLogoTwitter.svelte'
-import IoLogoGitHub from 'svelte-icons/io/IoLogoGitHub.svelte'
-import IoLogoLinkedIn from 'svelte-icons/io/IoLogoLinkedIn.svelte'
-
 </script>
 <style>
-.icon {
-    color: black;
-    width: 1.8em;
-    height: 1.8em;
-    display: inline-block;
-  }
-li {
-  text-decoration: none;
-  display: inline-block;
-}
 </style>
 
-<li><a href="https://twitter.com/mrgnw"><span class="icon"><IoLogoTwitter/></span></a></li>
-<li><a href="https://github.com/mrgnw"><span class="icon"><IoLogoGitHub/></span></a></li>
-<li><a href="https://linkedin.com/in/mrgnw"><span class="icon"><IoLogoLinkedIn/></span></a></li>
-<li><a href="https://instagram.com/mrgnw2"><span class="icon"><IoLogoInstagram/></span></a></li>
+<li><a href="https://twitter.com/mrgnw"><span>Twitter</span></a></li>
+<li><a href="https://github.com/mrgnw"><span>Github</span></a></li>
+<li><a href="https://linkedin.com/in/mrgnw"><span>LinkedIn</span></a></li>
+<li><a href="https://instagram.com/mrgnw2"><span>Instagram</span></a></li>
 
 
 
