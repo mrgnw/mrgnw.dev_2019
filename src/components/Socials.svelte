ce692cd7ef2735e14d51ea059c4d222d5a26e617
Morgan <morganfwilliams@gmail.com>
2020-01-17 01:12:21 -0500

    social links

--- src/components/Socials.svelte
+++ src/components/Socials.svelte
@@ -1,12 +1,16 @@
 <script>
 </script>
 <style>
+li {
+  text-decoration: none;
+  display: inline-block;
+}
+li + li:before {
+    content: " • ";
+}
 </style>
 
-<li><a href="https://twitter.com/mrgnw"><span>Twitter</span></a></li>
 <li><a href="https://github.com/mrgnw"><span>Github</span></a></li>
+<li><a href="https://twitter.com/mrgnw"><span>Twitter</span></a></li>
 <li><a href="https://linkedin.com/in/mrgnw"><span>LinkedIn</span></a></li>
 <li><a href="https://instagram.com/mrgnw2"><span>Instagram</span></a></li>
-
-
-
