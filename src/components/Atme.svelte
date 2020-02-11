a02ccc59725bb9ff0aadcf31bdc9fa2956c24c8f
Morgan <morganfwilliams@gmail.com>
2020-02-11 01:05:39 -0500

    Update Atme.svelte

--- src/components/Atme.svelte
+++ src/components/Atme.svelte
@@ -4,6 +4,7 @@
 	$: encoded = encodeURI(text)
 	let prefix = "https://twitter.com/intent/tweet?text="
 	$: link = `${prefix}${text}`
+	// todo: automatically add location?
 </script>
 
 <a href="{link}">
