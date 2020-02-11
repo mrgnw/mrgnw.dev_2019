024746fb769533ce6e81e98d5731e74a3ff5dadb
Morgan <morganfwilliams@gmail.com>
2020-02-11 01:05:30 -0500

    @ me!
    You can tell that it's an @ me because of the way it is

---
+++ src/components/Atme.svelte
@@ -0,0 +1,11 @@
+
+<script>
+	export let text = ""
+	$: encoded = encodeURI(text)
+	let prefix = "https://twitter.com/intent/tweet?text="
+	$: link = `${prefix}${text}`
+</script>
+
+<a href="{link}">
+	<slot>@ me!</slot>
+</a>
